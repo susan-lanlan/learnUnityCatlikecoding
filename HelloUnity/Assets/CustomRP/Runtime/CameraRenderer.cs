2851faefb4ca93b20b1e4a0f62456d64127b3e48
susan_lanlan@qq.com <susan_lanlan@qq.com>
2023-01-14 23:00:19 +0800

    catlikecoding Tutorial 1

---
+++ HelloUnity/Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+
+    ScriptableRenderContext context;
+    Camera camera;
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+    CullingResults cullingResults;
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        //设置相机清除状态
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,  //是否要清除深度缓冲
+            flags == CameraClearFlags.Color,  //是否要清除颜色缓冲
+            flags == CameraClearFlags.Color ? //设置用于清除缓冲区的颜色值
+                camera.backgroundColor.linear : Color.clear
+        );
+        //buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+
+    }
+    void DrawVisibleGeometry()
+    {
+        //设置绘制顺序和指定渲染相机
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        //设置渲染的Shader Pass和渲染排序
+        var drawingSettings = new DrawingSettings(unlitShaderTagId,sortingSettings);
+        //只绘制RenderQueue为opaque不透明的物体
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        //1.绘制不透明物体
+        context.DrawRenderers(cullingResults,ref drawingSettings,ref filteringSettings);
+        //2.绘制天空盒
+        context.DrawSkybox(camera);
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        //只绘制RenderQueue为transparent透明的物体
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        //3.绘制透明物体
+        context.DrawRenderers(cullingResults,ref drawingSettings,ref filteringSettings);
+    }
+    
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
+
