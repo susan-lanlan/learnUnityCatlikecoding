82b8e9c64164e985021b55219f7234d9287fc35f
susan_lanlan@qq.com <susan_lanlan@qq.com>
2023-01-16 16:03:46 +0800

    Tutorial 2:shader,SRP batcher,GPU instancing

---
+++ DrawCalls/Assets/CustomRP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline 
+{
+
+    CameraRenderer renderer = new CameraRenderer();
+    //≤‚ ‘SRP∫œ≈˙∆Ù”√
+    /*public CustomRenderPipeline()
+    {
+        GraphicsSettings.useScriptableRenderPipelineBatching = true;
+    }*/
+
+    protected override void Render(
+        ScriptableRenderContext context, Camera[] cameras
+    )
+    {
+        foreach (Camera camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+
+
+}
