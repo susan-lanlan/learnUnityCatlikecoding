0ee4608c5f0ab58eb421c7a3fc1928f5ad1c768f
susan_lanlan@qq.com <susan_lanlan@qq.com>
2023-02-07 22:12:41 +0800

    PCF soft shadow

---
+++ ShadowPCFTransparency/Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+
+    ScriptableRenderContext context;
+    Camera camera;
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    CullingResults cullingResults;
+
+    static ShaderTagId
+    unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit"),
+    litShaderTagId = new ShaderTagId("CustomLit");
+
+    Lighting lighting = new Lighting();
+
+    public void Render(ScriptableRenderContext context, Camera camera,
+        bool useDynamicBatching, bool useGPUInstancing,
+        ShadowSettings shadowSettings)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull(shadowSettings.maxDistance))
+        {
+            return;
+        }
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+        lighting.Setup(context, cullingResults, shadowSettings);
+        buffer.EndSample(SampleName);
+        Setup();
+        DrawVisibleGeometry(useDynamicBatching, useGPUInstancing);
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        lighting.Cleanup();
+        Submit();
+    }
+    bool Cull(float maxShadowDistance)
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            p.shadowDistance = Mathf.Min(maxShadowDistance, camera.farClipPlane);
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        //设置相机清除状态
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,  //是否要清除深度缓冲
+            flags == CameraClearFlags.Color,  //是否要清除颜色缓冲
+            flags == CameraClearFlags.Color ? //设置用于清除缓冲区的颜色值
+                camera.backgroundColor.linear : Color.clear
+        );
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+
+    }
+    void DrawVisibleGeometry(bool useDynamicBatching, bool useGPUInstancing)
+    {
+        //设置绘制顺序和指定渲染相机
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        //设置渲染的Shader Pass和渲染排序
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings)
+        {
+            enableDynamicBatching = useDynamicBatching,
+            enableInstancing = useGPUInstancing
+        };
+        drawingSettings.SetShaderPassName(1, litShaderTagId);
+        //只绘制RenderQueue为opaque不透明的物体
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        //1.绘制不透明物体
+        context.DrawRenderers(cullingResults,ref drawingSettings,ref filteringSettings);
+        //2.绘制天空盒
+        context.DrawSkybox(camera);
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        //只绘制RenderQueue为transparent透明的物体
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        //3.绘制透明物体
+        context.DrawRenderers(cullingResults,ref drawingSettings,ref filteringSettings);
+    }
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
+
